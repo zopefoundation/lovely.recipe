be9e8d9f72b9c4dafa57851aa111fe5a8d75a45a
Jodok Batlogg <jodok@batlogg.com>
2007-08-28 15:55:36 +0000

    bumping version to release alpha egg

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ server = lovely.recipe.zeo:LovelyServer
 
 setup (
     name='lovely.recipe',
-    version='0.3.0a1',
+    version='0.3.1a1',
     author = "Lovely Systems",
     author_email = "office@lovelysystems.com",
     license = "ZPL 2.1",
